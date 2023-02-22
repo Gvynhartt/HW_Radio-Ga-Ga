b05623ed3dc13575ab35395aa0db3c95fa22fab7
Gvynhartt <savshush134@gmail.com>
2023-02-22 13:45:46 +0300

    constructors and new tests added for Radio.java

--- src/main/java/ru/netology/mein/Radio/Radio.java
+++ src/main/java/ru/netology/mein/Radio/Radio.java
@@ -6,18 +6,40 @@ public class Radio {
      * сохраняет номер станции [0...9]
      */
 
+    private int totalStations;
+    /**
+     * поле с числом станций, необходимое для конструктора
+     */
+
     private int volSound;
 
     /**
      * сохраняет громкость звука [0...10]
      */
+    private int minVolSound = 0;
+    /**
+     * ПЕРЕДЕЛКА ГРОМКОСТИ ПРЕДПОЛАГАЕТ КОНСТРУКТОР ИЛИ НЕТ?
+     */
+    private int maxVolSound = 100;
+
+    /**
+     * ТОТ ЖЕ ВОПРОС
+     */
+
+    public Radio(int totalStations) { /** параметризованный конструктор с указанием числа станций*/
+        this.totalStations = totalStations;
+    }
+
+    public Radio() { /** НЕпараметризованный конструктор, предполагающий число станции по умолчанию (10)*/
+        totalStations = 10;
+    }
 
     public int getNmbStation() { /** геттер для номера станции*/
         return nmbStation;
     }
 
     public int setNmbStation(int newNmbStation) { /** сеттер (НЕтривиальный) для номера станции*/
-        if (newNmbStation >= 0 && newNmbStation <= 9) {
+        if (newNmbStation >= (totalStations - totalStations) && newNmbStation < totalStations) {
             nmbStation = newNmbStation;
         } else {
             nmbStation = getNmbStation(); /** в задании не прописано, какое поведение предполагается
@@ -35,33 +57,34 @@ public class Radio {
     }
 
     public int increaseVolume() { /** метод для увеличения громкости на 1*/
-        if (volSound < 10) {
+        if (volSound < maxVolSound) {
             volSound = volSound + 1;
         }
         return volSound;
     }
 
     public int decreaseVolume() { /** метод для уменьшения громкости на 1*/
-        if (volSound > 0) {
+        if (volSound > minVolSound) {
             volSound = volSound - 1;
         }
         return volSound;
     }
 
     public int nextStation() { /** метод для переключения станции на следующую (по кнопке)*/
-        if (nmbStation < 9) {
+        if (nmbStation < (totalStations - 1)) {
             nmbStation = nmbStation + 1;
         } else {
-            nmbStation = 0;
+            nmbStation = totalStations - totalStations; /** как альтернатива простому указанию 0,
+             т.е. первой станции ПО НОМЕРУ*/
         }
         return nmbStation;
     }
 
     public int previousStation() { /** метод для переключения станции на предыдущую (по кнопке)*/
-        if (nmbStation > 0) {
+        if (nmbStation >= 1) {
             nmbStation = nmbStation - 1;
         } else {
-            nmbStation = 9;
+            nmbStation = totalStations - 1; /** переключает на последнюю станцию ПО НОМЕРУ*/
         }
         return nmbStation;
     }
